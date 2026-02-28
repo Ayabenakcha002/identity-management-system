c43f8f1024f6308fbe9f86140e719981f697c753
aya <ayabenakcha002@gmail.com>
2026-02-28 14:30:00 +0100

    add admin registration and model tests

--- identity/admin.py
+++ identity/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Person, Student, Faculty, Staff, StatusHistory, IdentityLink
 
-# Register your models here.
+
+admin.site.register(Person)
+admin.site.register(Student)
+admin.site.register(Faculty)
+admin.site.register(Staff)
+admin.site.register(StatusHistory)
+admin.site.register(IdentityLink)
+
+
